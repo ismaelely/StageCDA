745b73a2d062860925b8bf2fcbf5730edbd51e89
Ismael <ismaelelyattaoui@gmail.com>
2022-06-21 13:33:01 +0200

    Partie Detail Blog: - Blog finit, ya un problemme a regler, des le titre 5, il ne me laisse pas afficher l'user.
    Partie Detail Actu:
    - Tout marche sauf une chose, des que j'arrive au 5ieme id il me laisse pas afficher l'user.

--- src/Controller/ViewActuController.php
+++ src/Controller/ViewActuController.php
@@ -2,19 +2,65 @@
 
 namespace App\Controller;
 
+use App\Entity\CommentsActu;
+use App\Form\CommentsActuType;
+use App\Repository\ActualitesRepository;
+use App\Repository\BlogRepository;
+use App\Repository\CommentsActuRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class ViewBlogController
+ * @package App\Controller
+ * @Route("/actualites", name="actualites_")
+ */
 
 class ViewActuController extends AbstractController
 {
     /**
-     * @Route("/actualites/vue", name="app_view_actu")
+     * @Route("/{id}", name="app_view_actualites")
      */
-    public function index(): Response
+    public function index(int $id,
+                          ActualitesRepository $actuRepository,
+                          UserRepository $userRepository,
+                          CommentsActuRepository $commentsActuRepository,
+                          Request $request
+    ): Response
     {
+        $actus= $actuRepository->find($id);
+
+        #POUR FORMULAIRE DE COMMENTAIRES#
+        $actuComment = new CommentsActu();
+        $form = $this->createForm(CommentsActuType::class, $actuComment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            #ON SET DATE D'AUJOURD'HUI ET L'ETAT A 0#
+            $actuComment->setDate(new \DateTimeImmutable('now'));
+            $actuComment->setEtat("0");
+
+            #SA SERT POUR QUE LE COMMENTAIRE OBTIEN L'ID DU BLOG#
+            $id = $actuRepository->find($id);
+            $actuComment->setActu($id);
+
+            $em->persist($actuComment);
+            $em->flush();
+        }
+
+
         return $this->render('view_actu/index.html.twig', [
-            'controller_name' => 'ViewActuController',
+            "actus" => $actus,
+            'allActus'=> $actuRepository -> findAll(),
+            'user_actu' => $userRepository ->find($id),
+            'comments' => $commentsActuRepository ->findAll(),
+            'other' => $actuRepository ->findAll(),
+            'commentBlog' => $form->createView()
         ]);
     }
 }
